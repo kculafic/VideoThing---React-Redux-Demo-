404b954bc30b562392d9fa04aae0ec8566e018c2
Krsto Culafic <kculafic@gmail.com>
2017-12-06 12:01:40 -0800

    Add search bar component with basic input event handler

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyAqZGujr3hKG6uGcUe-RqFo6pA94GxFVeg';
 
 // Create a new component, which should produce some HTML
 const App = () => {
-  return <div>VideoThing</div>;
+  return (
+    <div>
+      <h1>VideoThing</h1>
+      <SearchBar />
+    </div>
+  )
 }
 
 // Take this component's generated HTML and render it in the DOM
